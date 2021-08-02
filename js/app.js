29ce08531107fe804e3a0f435487bf384848bddd
Dojando <wadja.b@hotmail.fr>
2021-08-02 19:26:21 +0200

    mise en forme du dropdown dans la page photographe

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// récupération des éléments du DOM
+const custom_dropdown = document.getElementById('custom_dropdown');
+const dropdown_options = document.getElementById('dropdown_options');
+const arrow = document.getElementById('arrow');
+
+// script pour le menu dropdown de la page photographe
+custom_dropdown.addEventListener('click', function(e) {
+  if (getComputedStyle(dropdown_options).display == "none") {
+    dropdown_options.style.display = "flex";
+    arrow.setAttribute('src', 'img/arrow_open.png');
+  }
+  else if (getComputedStyle(dropdown_options).display == "flex") {
+    dropdown_options.style.display = "none";
+    arrow.setAttribute('src', 'img/arrow_closed.png');
+  }
+})
